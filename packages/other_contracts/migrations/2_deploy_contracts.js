2262c186c179a665657e16db74433700dbea90a4
echo <hujw77@gmail.com>
2021-04-15 19:36:17 +0800

    fix setup order

--- packages/other_contracts/migrations/2_deploy_contracts.js
+++ packages/other_contracts/migrations/2_deploy_contracts.js
@@ -2,12 +2,12 @@ const WETH9 = artifacts.require("WETH9")
 const MULTICALL = artifacts.require("Multicall")
 const ROUTER  = artifacts.require("UniswapV2Router02")
 const fs = require('fs')
-const contracts = require('../../../uniswap-v2-sdk/contract_addresses.json')
+const contracts = require('../../uniswap-v2-sdk/contract_addresses.json')
 module.exports = async function(deployer) {
     await deployer.deploy(MULTICALL);
     const multicallContract = await MULTICALL.deployed()
     await deployer.deploy(WETH9);
-    const weth9Contract = await  WETH9.deployed()
+    const weth9Contract = await WETH9.deployed()
     await deployer.deploy(ROUTER, contracts.factoryAddress, weth9Contract.address);
     const  routerContract = await ROUTER.deployed()
     fs.writeFileSync('../../packages/uniswap-v2-sdk/contract_addresses.json',JSON.stringify({
